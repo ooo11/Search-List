da1c61c2e7f416dd8f1797fb211a6d7337e1057e
ooo11 <markmizuro@gmail.com>
2020-05-22 15:22:55 +0800

    Add files via upload

--- models/Registration.js
+++ models/Registration.js
@@ -1,14 +1,43 @@
 const mongoose = require('mongoose');
+const cors = require('cors');
+mongoose.set('useCreateIndex', true);
+
 
 const registrationSchema = new mongoose.Schema({
-  name: {
+  PartName: {
     type: String,
     trim: true,
   },
-  email: {
+  PartNo: {
     type: String,
     trim: true,
   },
+  Quantity: {
+    type: String,
+    trim: true,
+  },
+  DrawingNo:{
+    type: String,
+    trim: true,
+  },
+  Group:{
+    type: String,
+    trim: true,
+  },
+  Model:{
+    type: String,
+    trim: true,
+  },
+  Tags:{
+    type: Array,
+    trim: true,
+  }
+
 });
 
+
+
+registrationSchema.index({ PartName: 'text' });
+
 module.exports = mongoose.model('Registration', registrationSchema);
+
