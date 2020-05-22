fbe82d5bdc9f9499e6130916d9ca03a1bbb677a2
ooo11 <markmizuro@gmail.com>
2020-05-22 15:15:04 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+//app.use(express.static('public'));
+// Static
+app.use('/public', express.static(path.join(__dirname, '.', 'public')));
+
 app.use('/', routes);
+
+
 
 module.exports = app;
